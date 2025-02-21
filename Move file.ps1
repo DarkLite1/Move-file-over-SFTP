8973a80a0334083cf60882d0b67897fffde04996
DarkLite1 <brechtgijbels@gmail.com>
2025-02-21 11:49:36 +0100

    rename error

--- Move file.ps1
+++ Move file.ps1
@@ -601,7 +601,7 @@ try {
                             }
                         
                             if ($duplicateFileInLocalTempFolder) {
-                                Save-ErrorMessageHC "Duplicate file in the local temp folder '$($localTempDownloadFolder)', most likely due to the file being in use in the destination folder during the previous run, use OverwriteFile if desired'"
+                                Save-ErrorMessageHC "Duplicate file in the local temp folder '$($localTempDownloadFolder)', most likely due to the file being in use in the destination folder during the previous run, use OverwriteFile if desired"
 
                                 continue
                             }
