e80089ee7f59e21c703149443f5a299647d3330e
DarkLite1 <brechtgijbels@gmail.com>
2025-02-27 08:58:53 +0100

    update error message

--- Move file.ps1
+++ Move file.ps1
@@ -455,7 +455,7 @@ try {
                         $result.Moved = $true
                     }
                     catch {
-                        Save-ErrorMessageHC "Failed to move the previously downloaded file to the destination folder: $_"
+                        Save-ErrorMessageHC "Failed to move the previously downloaded file '$($params.LiteralPath)' to the destination folder: $_"
 
                         $Error.RemoveAt(0)
                     }
