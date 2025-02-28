c86e3440b5827c36340ab60227fe12d9e29710b4
DarkLite1 <brechtgijbels@gmail.com>
2025-02-28 11:17:46 +0100

    fix file filter for upload

--- Move file.ps1
+++ Move file.ps1
@@ -820,7 +820,7 @@ try {
 
                 #region Select files to upload
                 try {
-                    $filesToUpload = $localFolderContent.tempFolder + $localFolderContent.rootFolder 
+                    $filesToUpload = $localFolderContent.tempFiles + $localFolderContent.rootFiles
 
                     if ($FileExtensions) {
                         Write-Verbose "Select files with extension '$FileExtensions'"
